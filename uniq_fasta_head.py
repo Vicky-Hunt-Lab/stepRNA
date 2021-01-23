0594c44aca9215bae618bc62c3bdbcdcd164631f
bmm41 <bmm41@bath.ac.uk>
2021-01-23 09:29:47 +0000

    Adding convert Fasta headers to unique names

---
+++ uniq_fasta_head.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from Bio import SeqIO
+import os
+from subprocess import run
+
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description='Convert fasta headers from non-unique headers to read[x]')
+
+parser.add_argument('-f', '--fasta', help='Fasta files to make unique headers')
+parser.add_argument('-n', '--name', default = 'Read', help='Basename for header')
+
+args = parser.parse_args()
+
+fasta_in = args.fasta
+name = args.name
+
+fasta_out = os.path.splitext(fasta_in)[0] + '_corrected' + os.path.splitext(fasta_in)[1]
+
+with open(fasta_in) as fin, open(fasta_out, 'w') as fout:
+    records = SeqIO.parse(fin, 'fasta')
+    for x, record in enumerate(records):
+        record.id = name + '_{}'.format(x) 
+        record.description = ''
+        SeqIO.write(record, fout, 'fasta')
+
