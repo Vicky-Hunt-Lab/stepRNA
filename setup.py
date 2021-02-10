82e4484f7acb692405b7a001ba07e043ce2f3e08
bmm41 <bmm41@bath.ac.uk>
2021-02-10 17:10:41 +0000

    setup.py is now working

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='stepRNA',
+    version='0.0.1',
+    authour='Ben Murcott',
+    authour_email='bmm41@bath.ac.uk',
+    description='Align short RNA seqeuncing reads to determine the length of of overhang.',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url='https://github.com/bmm514/stepRNA.git',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Licesnce :: OSI Approved :: MIT Licence",
+        "Operating System :: Ubuntu",
+    ],
+    #packages=setuptools.find_packages(),
+    packages=['stepRNA'],
+    python_requires='>=3.6',
+)
