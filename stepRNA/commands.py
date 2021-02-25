59beebdfd5e79a06c351b65144bebc74fe449463
bmm514 <benmurcott96@gmail.com>
2021-02-25 13:55:02 +0000

    Fix to allow outputting to a different directory to input files

--- stepRNA/commands.py
+++ stepRNA/commands.py
@@ -9,15 +9,15 @@ def left_overhang(sorted_bam, line, ref_positions):
     
     Returns: The length of the left overhang and the type of overhang'''
     if ref_positions[0] == 0:
-        return 0, 'LE'
+        return 0, '5primeExact'
     elif ref_positions[0] == None:
         if line.reference_start != 0:
             raise Exception
         else:
-            return line.query_alignment_start, 'LQ'
+            return line.query_alignment_start, '5primeRead'
     else:
         #If reference_position[0] > 0
-        return -ref_positions[0], 'LR'
+        return -ref_positions[0], '5primeRef'
 
 def right_overhang(sorted_bam, line, ref_positions):
     '''Get the length of the right overhang; 0 = no overhang, -ve = reference overhang, +ive = query overhang
@@ -29,15 +29,15 @@ def right_overhang(sorted_bam, line, ref_positions):
     Returns: The length of the left overhang and the type of overhang'''
     ref_length = sorted_bam.lengths[sorted_bam.get_tid(line.reference_name)]
     if ref_positions[-1] == ref_length - 1:
-        return 0, 'RE'
+        return 0, '3primeExact'
     elif ref_positions[-1] == None:
         if line.reference_end != ref_length:
            raise Exception
         else:
-            return ref_length - line.query_alignment_length , 'RQ'
+            return ref_length - line.query_alignment_length , '3primeRead'
             #return ref_length - line.query_length, 'RQ'
     else:
         #If reference_position[-1] < ref_length
-        return ref_positions[-1] - (ref_length - 1), 'RR'
+        return ref_positions[-1] - (ref_length - 1), '3primeRef'
 
 
