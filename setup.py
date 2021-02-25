a7987b4546165bd45268345b70f8b7d2c4c3ceb6
bmm514 <benmurcott96@gmail.com>
2021-02-25 15:59:47 +0000

    Updating setup.py to 0.0.10

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='stepRNA',
-    version='0.1.9',
+    version='0.1.10',
     author='Ben Murcott',
     author_email='bmm41@bath.ac.uk',
     description='Align short RNA seqeuncing reads to determine the length of of overhang.',
