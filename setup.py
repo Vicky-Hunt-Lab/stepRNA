3d13ca1303b0386ecfff370f68723a4c71018694
Ben Murcott <bmm41@bath.ac.uk>
2022-02-07 15:48:10 +0000

    Dist made for v1.0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='stepRNA',
-    version='1.0.4',
+    version='1.0.5',
     author='Ben Murcott',
     author_email='bmm41@bath.ac.uk',
     description='Align short RNA seqeuncing reads to determine the length of of overhang.',
