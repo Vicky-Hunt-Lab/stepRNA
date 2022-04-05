6e0c3256fd6c0c2b6aee1ea5b9b8e11f6b4a6fbe
Ben Murcott <bmm41@bath.ac.uk>
2022-04-05 15:03:47 +0100

    Double plotting functionality

--- stepRNA/plotting.py
+++ stepRNA/plotting.py
@@ -1,11 +1,13 @@
 import csv
+from os import path
 
 import matplotlib.pyplot as plt
 
-def process_overhang_csv(overhang_csv, end = 'five'):
+def process_overhang_csv(overhang_csv, end = 'five', library_size = None, add_overhang_csv = None, add_library_size = None):
     valid_ends = {'five', 'three'}
     if end not in valid_ends:
         raise ValueError(f'Invalid "end" option, select one of:\n\t {", ".join(valid_ends)}')
+
 
     if end == 'five':
         end_col = 1
@@ -14,31 +16,65 @@ def process_overhang_csv(overhang_csv, end = 'five'):
     with open(overhang_csv) as csvin:
         csvreader = csv.reader(csvin)
         header_info = next(csvreader)
-        xvals, yvals = process_lines(csvreader, end_col)
+        xvals, yvals = process_lines(csvreader, end_col, library_size)
+
+    if add_overhang_csv is None:
+        return xvals, yvals
+
+    with open(add_overhang_csv) as csvin:
+        print('addtiional')
+        csvreader = csv.reader(csvin)
+        header_info = next(csvreader)
+        add_xvals, add_yvals = process_lines(csvreader, end_col, add_library_size)
+
+    xvals = [xvals, add_xvals]
+    yvals = [yvals, add_yvals]
 
     return xvals, yvals
 
 
-def process_lines(csvreader, end_col):
+def process_lines(csvreader, end_col, library_size = None):
     xvals = []
     yvals = []
     for line in csvreader:
-        xvals.append(int(line[0]))
-        yvals.append(int(line[end_col]))
+        x = int(line[0])
+        y = int(line[end_col])
+        if library_size is not None:
+            y = normalise(y, library_size)
+        xvals.append(x)
+        yvals.append(y)
     return xvals, yvals
 
-def make_plot(xvals, yvals, figname):
+def normalise(count, library_size):
+    scaling_factor = library_size / 1000000
+    return count / scaling_factor
+
+def make_plot(xvals, yvals, figname, width):
     try:
-        plt.bar(xvals, yvals)
+        plt.bar(xvals, yvals, width)
         plt.xticks(xvals)
         plt.savefig(figname)
     finally:
         plt.close()
 
+def make_double_plot(xvals, yvals, figname, width, legend_names):
+    try:
+        plt.bar(xvals[0], yvals[0], align = 'edge', width = width)
+        plt.bar(xvals[1], yvals[1], align = 'edge', width = -width)
+        plt.xticks(xvals[0])
+        plt.legend(legend_names)
+        plt.savefig(figname)
+    finally:
+        plt.close()
 
-def main(overhang_csv, end, figname):
-    xvals, yvals = process_overhang_csv(overhang_csv, end)
-    make_plot(xvals, yvals, figname)
+def main(overhang_csv, end, figname, library_size, width, add_overhang_csv, add_library_size):
+    xvals, yvals = process_overhang_csv(overhang_csv, end, library_size, add_overhang_csv, add_library_size)
+    if add_overhang_csv is None:
+        make_plot(xvals, yvals, figname, width)
+    else:
+        legend_1 = path.splitext(path.basename(overhang_csv))[0]
+        legend_2 = path.splitext(path.basename(add_overhang_csv))[0]
+        make_double_plot(xvals, yvals, figname, width, (legend_1, legend_2))
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
@@ -49,7 +85,17 @@ if __name__ == '__main__':
     parser.add_argument('output', help = 'Figure output filepath')
     parser.add_argument('-d', '--duplex_end', help = 'End of duplex to plot. Chose from: five or three',
             default = 'five', choices = {'five', 'three'})
+    parser.add_argument('-s', '--library_size', help = 'Optional RPM normalisation using library size', 
+            default = None, type = int)
+    parser.add_argument('-w', '--width', help = 'Bar plot width', default = 0.8, type = float)
+    parser.add_argument('-a', '--add_overhang_csv', help = 'Additonal overhang csv to plot alongside', 
+            default = None)
+    parser.add_argument('-S', '--add_library_size', help = 'Optional RPM normalisation for additional overhang csv using library size', 
+            default = None, type = int)
 
     args = parser.parse_args()
 
-    main(args.overhang_csv, args.duplex_end, args.output)
+    if (args.library_size is not None) and (args.add_library_size is None):
+        raise ValueError(f'Please specify an additional library size using -S')
+
+    main(args.overhang_csv, args.duplex_end, args.output, args.library_size, args.width, args.add_overhang_csv, args.add_library_size)
